41c3df3cd59d047d9013be8e017b63130fb54551
Gonzalo <Gonzalo@ACER-FO200-PC>
2014-06-13 15:32:56 +0200

    GUI update

--- src/es/unileon/barcode/GUI/MainWindow.java
+++ src/es/unileon/barcode/GUI/MainWindow.java
@@ -7,11 +7,13 @@
 package es.unileon.barcode.GUI;
 
 import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 /**
@@ -20,17 +22,20 @@ import javax.swing.JTextField;
  */
 public class MainWindow extends JFrame{
     
-    private JTextField barcode;
-    
-    private JButton generateValidBarcode;
-    private JButton generateBarcodeDeletedDigit;
-    private JButton generateBarcodeChangedDigit;
-    
     private BorderLayout borderLayout;
     
     private JMenuBar menuBar;
     private JMenu dataMenu;
-    private JMenuItem setBarcode;
+    private JMenuItem setPercentage;
+    private JMenuItem setBarcodeLength;
+    
+    private JPanel textPanel;
+    private JTextField barcode;
+    
+    private JPanel buttonPanel;
+    private JButton generateValidBarcode;
+    private JButton generateBarcodeDeletedDigit;
+    private JButton generateBarcodeChangedDigit;
     
     public MainWindow(){
         this.setSize(700, 500);
@@ -39,14 +44,25 @@ public class MainWindow extends JFrame{
         this.setLayout(borderLayout);
         menuBar = new JMenuBar();
         this.add(menuBar, BorderLayout.NORTH);
-        dataMenu = new JMenu("Data");
+        dataMenu = new JMenu("Datos");
         menuBar.add(dataMenu);
-        setBarcode = new JMenuItem("Set Barcode");
-        dataMenu.add(setBarcode);
+        setPercentage = new JMenuItem("Probabilidad de error");
+        dataMenu.add(setPercentage);
+        setBarcodeLength = new JMenuItem("Longitud");
+        dataMenu.add(setBarcodeLength);
         
-        JTextField text = new JTextField();
-        text.setText(this.getHeight() + "X" + this.getWidth());
-        this.add(text);
+        textPanel = new JPanel();
+        textPanel.setLayout(new GridBagLayout());
+        
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridBagLayout());
+        
+        this.add(textPanel, BorderLayout.WEST);
+        this.add(buttonPanel, BorderLayout.EAST);
+        
+        barcode = new JTextField();
+        barcode.setText(this.getHeight() + "X" + this.getWidth());
+        textPanel.add(barcode);
     }
     
 }
