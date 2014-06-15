5f8a5f9735f839db2f5a49a3653952a8718cd9f8
Eva <Eva@Eva-PC>
2014-06-15 12:04:38 +0200

    Added GUI

--- src/es/unileon/barcode/GUI/MainWindow.java
+++ src/es/unileon/barcode/GUI/MainWindow.java
@@ -32,7 +32,7 @@ public class MainWindow extends JFrame {
     private JMenu dataMenu;
     private JMenuItem setPercentage;
     private JMenuItem setBarcodeLength;
-    
+
     private JLabel correct;
     private JLabel incorrect;
     private JLabel empty;
@@ -45,6 +45,7 @@ public class MainWindow extends JFrame {
     private JButton generateValidBarcode;
     private JButton generateBarcodeDeletedDigit;
     private JButton generateBarcodeChangedDigit;
+    private JButton checkButton;
 
     public MainWindow() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -64,12 +65,12 @@ public class MainWindow extends JFrame {
 
         //Panel de los codigos de barras
         textPanel = new JPanel();
-        textPanel.setBackground(Color.red);
+        textPanel.setBackground(Color.white);
         textPanel.setLayout(new GridBagLayout());
 
         //Panel de los botones
         buttonPanel = new JPanel();
-        buttonPanel.setBackground(Color.blue);
+        buttonPanel.setBackground(Color.white);
         buttonPanel.setLayout(new GridBagLayout());
         GridBagConstraints consts = new GridBagConstraints();
 
@@ -80,63 +81,69 @@ public class MainWindow extends JFrame {
         this.getContentPane().add(textPanel, BorderLayout.CENTER);
         this.getContentPane().add(buttonPanel, BorderLayout.EAST);
 
-         //IMAGENES
+        //IMAGENES
         ImageIcon iconCorrect = new ImageIcon("images/correcto.png");
         ImageIcon iconIncorrect = new ImageIcon("images/incorrecto.png");
         ImageIcon iconVoid = new ImageIcon();
         correct = new JLabel(iconCorrect);
         incorrect = new JLabel(iconIncorrect);
         empty = new JLabel(iconVoid);
-        
+
         //PANEL IZQUIERDA
         consts.gridwidth = GridBagConstraints.RELATIVE;
-        consts.insets = new Insets(10,0,10,0);
+        consts.insets = new Insets(10, 0, 10, 0);
         consts.fill = GridBagConstraints.HORIZONTAL;
-        consts.ipadx=200;
-        consts.ipady=20;
+        consts.ipadx = 200;
+        consts.ipady = 20;
         barcodeGenerated = new JTextField("barcode");
-        
+        barcodeGenerated.setHorizontalAlignment(JTextField.CENTER);
+        barcodeGenerated.setBackground(new Color(235, 237, 237));
         textPanel.add(barcodeGenerated, consts);
+       
         consts.gridwidth = GridBagConstraints.REMAINDER;
         consts.fill = GridBagConstraints.NONE;
-        consts.ipadx=10;
-        textPanel.add(correct,consts);
+        consts.ipadx = 10;
+        textPanel.add(correct, consts);
 
         consts.gridwidth = GridBagConstraints.RELATIVE;
         barcodeResult = new JTextField("result");
+        barcodeResult.setHorizontalAlignment(JTextField.CENTER);
+        barcodeResult.setBackground(new Color(235, 237, 237));
+
         consts.fill = GridBagConstraints.HORIZONTAL;
-        consts.ipadx=200;
+        consts.ipadx = 200;
         textPanel.add(barcodeResult, consts);
         consts.gridwidth = GridBagConstraints.REMAINDER;
         consts.fill = GridBagConstraints.NONE;
-        consts.ipadx=10;
-        textPanel.add(incorrect,consts);
+        consts.ipadx = 10;
+        textPanel.add(incorrect, consts);
 
-        
-        
-       
-      
-        
         //PANEL DERECHA: BOTONES
-       
-        
         generateBarcodeChangedDigit = new JButton("Intercambio de digitos");
         generateBarcodeDeletedDigit = new JButton("Borron");
-        
+        generateValidBarcode = new JButton("Codigo de barras valido");
+        checkButton = new JButton("Test");
+        consts.ipadx = 80;
+        consts.ipady = 5;
         consts.insets = new Insets(10, 20, 10, 20);
         consts.gridwidth = GridBagConstraints.RELATIVE;
 
         consts.gridx = 2;
         consts.gridy = 0;
-        buttonPanel.add(generateBarcodeChangedDigit, consts);
+        buttonPanel.add(generateValidBarcode, consts);
+
         consts.gridwidth = GridBagConstraints.RELATIVE;
         consts.gridx = 2;
         consts.gridy = 1;
+        buttonPanel.add(generateBarcodeChangedDigit, consts);
+        consts.gridwidth = GridBagConstraints.RELATIVE;
+        consts.gridx = 2;
+        consts.gridy = 2;
         buttonPanel.add(generateBarcodeDeletedDigit, consts);
-
-       
-
-        
+        consts.gridwidth = GridBagConstraints.RELATIVE;
+        consts.gridx = 2;
+        consts.gridy = 3;
+        buttonPanel.add(checkButton, consts);
 
         this.setSize(700, 500);
         this.setVisible(true);
